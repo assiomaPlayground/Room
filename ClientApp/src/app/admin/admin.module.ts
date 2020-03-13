d346c594bdf677b8acf9c9f482c4470fc7e327db
lorenzo <lorenzo.fornabaio@gmail.com>
2020-03-13 15:22:19 +0100

    qrcode

--- ClientApp/src/app/admin/admin.module.ts
+++ ClientApp/src/app/admin/admin.module.ts
@@ -4,9 +4,12 @@ import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin.routing';
 import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { QRCodeGenComponent } from '../qrcode-gen/qrcode-gen.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent,
+  QRCodeGenComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
