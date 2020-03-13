7b130b4b8aaec8033c0d6e8b99e7a399e296de22
Your Name <d.bologna300@gmail.com>
2020-03-13 17:42:13 +0100

    update Encrypt and Decrypt

--- Utils/CryptProvider.cs
+++ Utils/CryptProvider.cs
@@ -20,9 +20,16 @@ namespace RoomService.Utils
 			SaltKey      = settings.SaltKey;
 			VIKey        = settings.VIKey;
 		}
+
+
 		public string Encrypt(string plainText)
 		{
+
+			// Convert our plaintext into a byte array.
+			// Let us assume that plaintext contains UTF8-encoded characters.
 			byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+
 
 			byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
 			var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
@@ -30,22 +37,39 @@ namespace RoomService.Utils
 
 			byte[] cipherTextBytes;
 
+			// Define memory stream which will be used to hold encrypted data.
 			using (var memoryStream = new MemoryStream())
 			{
+				// Define cryptographic stream (always use Write mode for encryption)
 				using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
 				{
+					// Start encrypting.
 					cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+
+					// Finish encrypting.
 					cryptoStream.FlushFinalBlock();
-					cipherTextBytes = memoryStream.ToArray();
+					// Convert our encrypted data from a memory stream into a byte array.
+
+                    cipherTextBytes = memoryStream.ToArray();
+
+					// Close both streams.
+
 					cryptoStream.Close();
 				}
 				memoryStream.Close();
 			}
+
+			// Convert encrypted data into a base64-encoded string.
+
 			return Convert.ToBase64String(cipherTextBytes);
 		}
+
 		public string Decrypt(string encryptedText)
 		{
-			byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+
+			// Convert our ciphertext into a byte array.
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+
 			byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
 			var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
 
@@ -54,8 +78,11 @@ namespace RoomService.Utils
 			var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
 			byte[] plainTextBytes = new byte[cipherTextBytes.Length];
 
-			int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-			memoryStream.Close();
+			// Start decrypting.
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+			
+			// Close both streams.
+            memoryStream.Close();
 			cryptoStream.Close();
 			return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
 		}
