c110a26520066d2e19ffa417cc8d9ec18d0ea6f9
Salvo Furnari <xsaf94x@gmail.com>
2020-03-24 11:15:38 +0100

    Dynamic hyde icon menu

--- ClientApp/src/app/shareds/icon-menu/icon-menu.component.ts
+++ ClientApp/src/app/shareds/icon-menu/icon-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-icon-menu',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconMenuComponent implements OnInit {
 
-  constructor() { }
+  IsVisible : boolean;
+  path : string;
+
+  toHide : string[] = ['/home', '/register', '/login']
+
+  constructor(private route : Router) { }
 
   ngOnInit() {
+    this.route.events.subscribe( evnt => {
+      if(evnt['routerEvent']) if(evnt['routerEvent'].url)
+      {
+        this.path = evnt['routerEvent'].url;
+        this.ValidateVisible();
+      }
+    })
+  }
+
+  private ValidateVisible(){
+    if(this.toHide.includes(this.path))
+      this.IsVisible = false;
+    else
+      this.IsVisible = true;
   }
 
 }
