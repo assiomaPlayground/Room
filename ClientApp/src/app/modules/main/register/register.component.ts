2bb2ee88e1260af79dc5ec2b01785cb06abaf8aa
Salvo Furnari <xsaf94x@gmail.com>
2020-03-24 14:51:41 +0100

    Sign and Login

--- ClientApp/src/app/modules/main/register/register.component.ts
+++ ClientApp/src/app/modules/main/register/register.component.ts
@@ -31,10 +31,8 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            username: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(6)]]
+            Username: ['', Validators.required],
+            Password: ['', [Validators.required, Validators.minLength(6)]]
         });
     }
 
@@ -51,11 +49,11 @@ export class RegisterComponent implements OnInit {
 
         this.loading = true;
         this.userService.register(this.registerForm.value)
-            .pipe(first())
             .subscribe(
                 data => {
                     this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
+                    this.authenticationService.login(data.Username, data.Password).subscribe(res =>
+                    this.router.navigate(['/home']));
                 },
                 error => {
                     this.alertService.error(error);
